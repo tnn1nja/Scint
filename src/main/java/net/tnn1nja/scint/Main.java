2204274b17875468fb2bfc3977e389a108635de2
tnn1nja <thenonsenseninja@gmail.com>
2023-11-30 15:51:41 +0000

    Outlined Random Pauser.

--- src/main/java/net/tnn1nja/scint/Main.java
+++ src/main/java/net/tnn1nja/scint/Main.java
@@ -1,11 +1,46 @@
 package net.tnn1nja.scint;
 
-public class Main {
-    public void onStart(){
+import java.util.Random;
 
+public class Main {
+
+    //Constants
+    Random rand = new Random();
+    int coeff = 1000;//*60;
+
+    //Variables
+    int minMins;
+    int maxMins;
+    int wait;
+
+
+    //Runs at Start Up
+    public void onStart(){
+        minMins = 5;
+        maxMins = 10;
+        newPause();
     }
 
-    public void onClose(){
+    //Generates New Pause Thread
+    public void newPause(){
+        wait = (rand.nextInt(maxMins*coeff) + minMins*coeff);
 
+        Thread timer = new Thread(() -> {
+            try{
+                Thread.sleep(wait);
+                playSound();
+                newPause();
+
+            } catch (InterruptedException e){
+                Thread.currentThread().interrupt();
+            }
+        });
+
+        timer.start();
+    }
+
+    //Plays a Sound
+    public void playSound(){
+        System.out.println("Noise");
     }
 }
